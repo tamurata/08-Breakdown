8b282e10694f44d30ff8a1b9a21b266c7eab02cd
tamurata <tamurata@iu.edu>
2019-11-11 22:12:30 -0500

    process

--- Scripts/ball.gd
+++ Scripts/ball.gd
@@ -1,6 +1,10 @@
 extends RigidBody2D
 
-export var maxspeed = 300
+export var maxspeed = 1000
+export var size_decay = 0.3
+export var alpha_decay = 0.03
+
+var count = 0
 
 signal lives
 signal score
@@ -15,6 +19,7 @@ func _ready():
 func _physics_process(delta):
  var bodies = get_colliding_bodies()
  for body in bodies:
+	get_parent().get_node("Camera").add_trauma(0.5)
   if body.is_in_group("Tiles"):
    emit_signal("score",body.score)
    body.queue_free()
@@ -28,3 +33,17 @@ func _physics_process(delta):
  if position.y > get_viewport_rect().end.y:
   emit_signal("lives")
   queue_free()
+
+var temp = $ColorRect.duplicate()
+	temp.rect_position = Vector2(position.x + $ColorRect.rect_position.x,position.y + $ColorRect.rect_position.y)
+	temp.name = "Trail" + str(count)
+	count += 1
+	$Trail.add_child(temp)
+	var trail = $Trail.get_children()
+	for t in trail:
+		t.rect_size = Vector2(t.rect_size.x-size_decay,t.rect_size.y-size_decay)
+		t.color.a -= alpha_decay
+		if t.color.a <= 0:
+			t.color.a = 0
+		if t.rect_size.x <= 0.5 or t.color.a <= 0:
+			t.queue_free()
